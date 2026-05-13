535b755a6d3e7878f8869f85c731f08374744b77
Lucas <lucas.hoffart2002@gmail.com>
2026-05-13 06:32:00 +0200

    Adding indicators to Dataframe with talib

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import ccxt
 import pandas as pd
+import talib
+
 
 exchange = ccxt.binance()
-ohlcv = exchange.fetch_ohlcv("ETH/USDT", timeframe="1m", limit=100)
+ohlcv = exchange.fetch_ohlcv("ETH/USDT", timeframe="1m", limit=250)
+
 
 df = pd.DataFrame(
     ohlcv,
     columns=["timestamp", "open", "high", "low", "close", "volume"]
 )
-
 df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+
+
+df["rsi"] = talib.RSI(df["close"], timeperiod=14)
+df["bb_haute"], df["bb_mid"], df["bb_basse"] = talib.BBANDS(df["close"], timeperiod=20)
+df["ema50"] = talib.EMA(df["close"], timeperiod=50)
+df["ema200"] = talib.EMA(df["close"], timeperiod=200)
+
 
 # print(df)
 # print(df["close"])
 # print(df["close"].iloc[-1])
 # print(df.tail())
+print(df[["timestamp", "close", "rsi", "bb_basse","ema200"]].tail())
